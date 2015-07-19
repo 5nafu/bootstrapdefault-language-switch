2db5d47d610329a6523b05e3ae885bd2fdf74bb1
Phil Bayfield <phil@bayfmail.com>
2015-07-19 09:25:26 +0100

    minor tweak to remove background image from text

--- main.inc.php
+++ main.inc.php
@@ -23,7 +23,7 @@
 
 /*
 Plugin Name: Bootstrap Default Language Switch
-Version: 2.7.2
+Version: 2.7.2.1
 Description: Switch to another language from flags on your gallery home page.
 Plugin URI: http://piwigo.org/ext/extension_view.php?eid=797
 Author: Phil Bayfield (philb)
